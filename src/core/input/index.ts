a1ff7b100a0cd470889c524080a1cd2c75672e14
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-19 00:57:18 -0300

    feat: deleting core content

--- src/core/input/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EmailInput } from "./ui/email-input.component";
-export { default as PasswordInput } from "./ui/password-input.component";
-export { default as TextInput } from "./ui/text-input.component";
--- src/core/input/shared/types/index.ts
+++
@@ -1,2 +0,0 @@
-import type * as InputTypes from "./input.types"
-export { InputTypes }
